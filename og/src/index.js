fcc251c2401d251c0146ab70cd25b6dee4fff486
David Kanter <david.kanter@herodigital.com>
2019-10-12 21:35:58 -0700

    Finished with section 'Asset Management.'

--- og/src/index.js
+++ og/src/index.js
@@ -1,10 +1,25 @@
 import _ from "lodash";
+import "./style.css";
+import Icon from "./icon.jpg";
+import Data from "./data.xml";
 
 function component() {
   const element = document.createElement("div");
 
   // Add a little Lodash
-  element.innerHTML = _.join(["Hello", "Dave!"], " ");
+  element.innerHTML = _.join(["Hello", "Dave!<br />"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our existing div.
+  // check out image-webpack-loader and url-loader for more
+  // information on enhancing the image loading process.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  // output that xml as JSON
+  console.log(Data);
 
   return element;
 }
--- og/webpack.config.js
+++ og/webpack.config.js
@@ -3,7 +3,37 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    //filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        // load css
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        // load images
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ["file-loader"]
+      },
+      {
+        // load fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        // load csv
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        // load xml
+        test: /\.xml$/,
+        use: ["xml-loader"]
+      }
+    ]
   }
 };
